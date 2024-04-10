36e07848fd257e0e4f1d71f41190cb1c6b4a7676
checkiamsiam <issiam02415@gmail.com>
2024-04-10 19:34:54 +0600

    Update components.json, package.json, page.js, and yarn.lock

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/components/home/Banner";
+import HomeContact from "@/components/home/HomeContact";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <HomeContact />
     </>
   );
 }
