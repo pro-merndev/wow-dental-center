6d799b9bbf9fb38747af82cca334c0da0f918f2b
checkiamsiam <issiam02415@gmail.com>
2024-04-11 00:44:33 +0600

    Add Welcome component to Home page

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -3,12 +3,14 @@ import FAQ from "@/components/home/FAQ";
 import Gallery from "@/components/home/Gallery";
 import HomeAbout from "@/components/home/HomeAbout";
 import HomeContact from "@/components/home/HomeContact";
+import Welcome from "@/components/home/Welcome";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <HomeAbout />
+      <Welcome />
       <Gallery />
       <FAQ />
       <HomeContact />
---
+++ src/components/home/Welcome.jsx
@@ -0,0 +1,39 @@
+import { Button } from "@/shadcn/ui/button";
+import SectionHeading from "../common/Section/SectionHeading";
+import InterfaceIcon from "../icons/InterfaceIcon";
+import Marquee from "react-fast-marquee";
+import Image from "next/image";
+
+const Welcome = () => {
+  return (
+    <div>
+      <div className="container">
+        <SectionHeading
+          subtitle="Welcome To"
+          title="Waves of Welcoming Dental Center"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+
+        <div className="flex justify-center gap-6 mt-10">
+          <Button className=" rounded-e-3xl rounded-es-3xl rounded-ss-none shadow-xl">
+            <InterfaceIcon height={15} width={15} />
+            <span className="font-copperPlate text-[20px] ml-3">Learn More</span>
+          </Button>
+          <Button className=" rounded-e-3xl rounded-es-3xl rounded-ss-none shadow-xl bg-primary">
+            <InterfaceIcon height={15} width={15} />
+            <span className="font-copperPlate text-[20px] ml-3">Schedule Appointment</span>
+          </Button>
+        </div>
+      </div>
+      <div className="py-20">
+      <Marquee autoFill >
+          <div className="relative sm:h-[184px] sm:w-[296px] h-[129px] w-[207px] mx-3">
+            <Image src="/images/welcome-image.png" alt="gallery" layout="fill" />
+          </div>
+        </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
