2f3c6ca94269c902b0c8933e65e12cee4b563a58
Mohammad Ali <itsproali@gmail.com>
2024-04-12 10:36:16 +0600

    Contact Section Responsive

--- src/shadcn/ui/input.jsx
+++ src/shadcn/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     (<input
       type={type}
       className={cn(
-        "flex h-9 w-full rounded-xl border border-input bg-white px-3 py-5 text-sm shadow-sm transition-colors file:border-0 file:bg-white file:text-sm file:font-medium placeholder:text-black placeholder:font-bold focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-9 w-full rounded-xl border border-input bg-white px-3 py-5 text-sm shadow-sm transition-colors file:border-0 file:bg-white file:text-sm file:font-medium placeholder:text-gray-[#9B9B9B] md:placeholder:text-black placeholder:font-bold focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       ref={ref}
