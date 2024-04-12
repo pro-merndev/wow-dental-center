83cc79fe97f5aa664111a6e7af5e2f1a426a50f2
Mohammad Ali <itsproali@gmail.com>
2024-04-12 16:53:05 +0600

    Update NavBar and Banner components for responsiveness

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -26,7 +26,7 @@ const actions = [
 const Banner = () => {
   return (
     <section
-      className="min-h-[1050px] flex items-center justify-center bg-cover bg-center bg-no-repeat py-28 md:py-40 text-white relative"
+      className="min-h-[1050px] flex items-center justify-center bg-cover bg-center bg-no-repeat pt-28 pb-36 md:py-40 text-white relative"
       style={{
         backgroundImage: "url(/images/banner_bg.png)",
       }}
@@ -47,7 +47,7 @@ const Banner = () => {
               </span>{" "}
               Dental Center
             </h1>
-            <p className="mt-5 max-w-[445px] text-lg max-md:text-base max-md:max-w-sm">
+            <p className="mt-5 max-w-[445px] text-lg max-md:text-sm max-md:max-w-sm w-[80%] font-extralight md:font-normal">
               Discover unparalleled dental care that transforms and revitalizes
               your smile with cutting-edge treatments in Fort Lauderdale.{" "}
             </p>
@@ -64,42 +64,55 @@ const Banner = () => {
             </div>
 
             <div className="block md:hidden my-8">
-              <button className="font-semibold bg-white text-black rounded-xl flex items-center px-2 text-sm">
-                <PhoneIcon size={12} />
+              <button className="font-semibold bg-white text-black md:rounded-[17px] rounded-[8px] flex items-center px-2 text-sm w-[202px] md:w-[217px]">
+                <PhoneIcon size={12} className="ml-[6px] md:ml-0" />
                 <div className="h-10 w-[1px] bg-black mx-4"></div>
-                <p>Call (754) 274 0675</p>
+                <p className="md:font-bold font-[800px]">Call (754) 274 0675</p>
               </button>
             </div>
           </div>
 
           {/* Right Side Form */}
           <div
-            className="w-full md:basis-5/12 bg-[#D9D9D9] bg-opacity-25 border border-white p-4 md:p-10"
+            className="md:w-full md:basis-5/12 bg-[#D9D9D9] bg-opacity-25 border border-white p-4 md:p-10 
+             w-full md:h-[515px] backdrop-blur-[5px] max-md:pb-6
+            "
             data-aos="fade-up"
           >
             <div className="flex flex-col gap-6 text-black">
-              <Input placeholder="Name" />
-              <Input placeholder="Email" />
-              <Input placeholder="Phone Number" />
+              <Input
+                placeholder="Name"
+                className="placeholder:text-black placeholder:font-bold pl-[20px]"
+              />
+              <Input
+                placeholder="Email"
+                className="placeholder:text-black placeholder:font-bold pl-[20px]"
+              />
+              <Input
+                placeholder="Phone Number"
+                className="placeholder:text-black placeholder:font-bold pl-[20px]"
+              />
               <Button className="bg-primary py-4 font-bold">
                 Schedule Appointment
               </Button>
             </div>
 
-            <div className="mt-12 flex flex-col md:flex-row items-center max-md:text-center justify-between gap-1 md:gap-4">
+            <div className="mt-[30px] md:mt-12 flex flex-col md:flex-row items-center max-md:text-center justify-between gap-1 md:gap-4">
               <div>
-                <p className="font-bold">New Patients, Good News</p>
-                <p className="max-w-[210px] md:max-w-[230px] mt-2">
+                <p className="md:font-bold font-normal tracking-[0.5px]">
+                  New Patients, Good News
+                </p>
+                <p className="max-w-[210px] md:max-w-[230px] mt-2 leading-4 md:font-normal font-thin tracking-[0.5px]">
                   To welcome new patients, we’re charging only
                 </p>
-                <p className="hidden md:block max-w-[230px] mt-1">
+                <p className="hidden md:block max-w-[230px] mt-1 leading-4 md:font-normal font-thin tracking-[0.5px]">
                   for a comprehensive dental exam, x-ray and teeth cleaning.
                 </p>
               </div>
-              <p className="text-[96px] max-md:leading-[96px] font-black text-white">
+              <p className="text-[96px] max-md:leading-[96px] font-black text-white my-3">
                 $99
               </p>
-              <p className="block md:hidden max-w-[220px]">
+              <p className="block md:hidden max-w-[230px] leading-4 md:font-normal font-thin">
                 for a comprehensive dental exam, x-ray and teeth cleaning.
               </p>
             </div>
@@ -108,7 +121,7 @@ const Banner = () => {
       </div>
 
       {/* Social Icons */}
-      <div className="absolute bottom-8 left-1/2 max-md:-translate-x-1/2 md:left-8">
+      <div className="absolute md:bottom-8 bottom-16 left-1/2 max-md:-translate-x-1/2 md:left-8">
         <div className="flex flex-row md:flex-col gap-7">
           <a href="">
             <FacebookIcon />
